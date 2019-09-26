ce1b37209aad2f3d4db1d43e86561aaaba0b3c49
leota <leotagit@gmail.com>
2019-09-26 10:06:15 +0200

    Updates

---
+++ precache-manifest.03ae714fa67e8106e8a7ca56efdb0967.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b93ca79e76cbecfdb99c31aee116b4",
+    "url": "/peaksjs-player/index.html"
+  },
+  {
+    "revision": "1f10feff12d9265ac1da",
+    "url": "/peaksjs-player/static/css/main.38d7129b.chunk.css"
+  },
+  {
+    "revision": "321aac32cc0b249f2ece",
+    "url": "/peaksjs-player/static/js/2.2e5ff58b.chunk.js"
+  },
+  {
+    "revision": "1f10feff12d9265ac1da",
+    "url": "/peaksjs-player/static/js/main.c828d7ab.chunk.js"
+  },
+  {
+    "revision": "dd1d553ab4b3e0126617",
+    "url": "/peaksjs-player/static/js/runtime-main.07f0f838.js"
+  },
+  {
+    "revision": "eceb32e3bec7a5cc3b3b378d3376c679",
+    "url": "/peaksjs-player/static/media/chant.eceb32e3.mp3"
+  },
+  {
+    "revision": "635755807175c7e3af67a82d6fee1691",
+    "url": "/peaksjs-player/static/media/electric-guitar.63575580.mp3"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/peaksjs-player/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "c9e4155a5a29203d6349a3598d4dc632",
+    "url": "/peaksjs-player/static/media/national-anthem.c9e4155a.mp3"
+  },
+  {
+    "revision": "31ef35dac5d0fd3e1cdba2751d00e803",
+    "url": "/peaksjs-player/static/media/sample.31ef35da.mp3"
+  },
+  {
+    "revision": "8a7680f19f640982f55a4e12a7ac89a0",
+    "url": "/peaksjs-player/static/media/testfile.8a7680f1.mp3"
+  }
+]);
